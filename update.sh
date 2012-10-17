fe3123175e6785fe644258f70d0077c0c9a8c2ab
dandaman <daniel.lang@biologie.uni-freiburg.de>
2012-10-17 17:52:49 +0200

    Nightly Build: Wed Oct 17 17:52:49 CEST 2012

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 d=$(date)
-git commit -a -q -uno -m "Nightly Build: $d" >> /dev/null
-git push -q origin master >> /dev/null
+git commit -a -q -uno -m "Nightly Build: $d" 
+git push -q origin master 
