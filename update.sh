98b00c09cf336cdb1ecbfa0334d8a2351e35d0ab
dandaman <daniel.lang@biologie.uni-freiburg.de>
2012-11-12 18:08:28 +0100

    Nightly Build: Mon Nov 12 18:08:07 CET 2012

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 d=$(date)
+cd /home/lang/www.cosmoss/annotation/goa/cosmoss-annotation/
 /home/lang/www.cosmoss/annotation/goa/cosmoss-annotation/make_distrib.sh
 git commit -a -m "Nightly Build: $d" > /home/lang/www.cosmoss/annotation/goa/cosmoss-annotation/update.log 2>&1
 git push origin master >> /home/lang/www.cosmoss/annotation/goa/cosmoss-annotation/update.log 2>&1
