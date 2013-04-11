a285a69b40376dda0c8ee44220270e068f827f4c
dandaman <daniel.lang@biologie.uni-freiburg.de>
2013-04-11 18:40:55 +0200

    added separate gaf2 for experimental evidenced GOAs

--- make_distrib.sh
+++ make_distrib.sh
@@ -9,7 +9,9 @@ psql -A -t -F'	' cosmoss -c " select accession,go_id,annotator_id,evidencecode,d
 psql -A -t -F'	' cosmoss -c "select 'cosmoss_PpV1.6' as db, accession as db_object_id,accession as db_object_symbol, '' as qualifier, value as go_id,case when (csid!= 0) then 'cosmoss_ref:'||csid else '' end as db_reference,evidencecode,'' as with,upper(substring(substring(name from E'\\\\S+\$'),1,1)) as aspect, (select distinct value from features where accession=F.accession and term_id = 10 and active=1 and version_id in (6,4,5) and status!=2) as db_object_name, '' db_object_synonym,'gene_product' as db_object_type, 'taxon:3218' as taxon, to_char(date,'YYYYMMDD') as date, 'cosmoss' as asigned_by, '' as annotation_extension, 'cosmoss_V1.6:'||accession as gene_product_form_id from features F join terms using (term_id) where active=1 and version_id in (6,4,5) and term_id in (4,5,6) and status != 2;"|sort -u > $NAME.gaf2
 echo "!gaf-version: 2.0" >temp
 cat temp $NAME.gaf2> temp2
+cat temp $NAME.gaf2 | grep -Pv '\tIEA|ISS\t' > temp3
 mv temp2 $NAME.gaf2
+mv temp3 $NAME.experimental_evidences.gaf2
 rm temp
 
 psql -A -F'	' cosmoss -c "select accession,features.annotator_id,coslink.csid, features.date,value, source from features left join coslink using (feature_id) where features.active=1 and version_id in (6,4,5) and term_id in (1) and features.status != 2 and ((coslink.active=1 and coslink.status !=2) or (coslink.feature_id is NULL)) group by accession,features.annotator_id,coslink.csid, features.date,value, source order by date desc;" |grep -v 'rows)' | perl -e 'while (<>) {chomp; my @a=split/\t/; if ($a[0] eq "accession") {@c=@a;next;} $l{$a[0]}{$a[2]}=\@a;} print join("\t", @c),"\n"; foreach my $p (sort keys %l) {my ($c)=keys %{$l{$p}}; my @b=@{$l{$p}{$c}}; $b[2]=join(",", keys %{$l{$p}}); print join("\t", @b),"\n";} ' > $NAME.gene_name.txt
